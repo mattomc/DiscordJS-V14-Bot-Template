560a014bda96dfc24dbd5ffc151b19f2b64cb0df
Matto MC <matthewmcguireclough770@gmail.com>
2024-01-23 00:44:13 +1030

    Update guildMemberAdd.js

--- src/events/Guild/guildMemberAdd.js
+++ src/events/Guild/guildMemberAdd.js
@@ -11,8 +11,10 @@ module.exports = {
      * @returns
      */
     run: async (member, client) => {
-      console.log(client.user)
       let SAFR_Guild = member.guilds.cache.find(g => g.id === '680036316783968316')
+      // @ts-ignore
+      let SAFR_New_Member = member.guilds.members.find(m => m.id === client.user.id)
+      console.log(SAFR_New_Member)
 
       if(SAFR_Guild.id !== '680036316783968316') return;
       let Channel = await SAFR_Guild.channels.cache.find(ch => ch.id === '685126748492529677');
